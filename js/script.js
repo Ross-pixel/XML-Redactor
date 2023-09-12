413750a8bb795a5821c45971b2bec4347c23da61
Ross-pixel <rosseroshenkov@yandex.ru>
2023-09-12 12:42:09 +0300

    add preview area

--- js/script.js
+++ js/script.js
@@ -40,11 +40,58 @@ function previewXml() {
   try {
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(xmlEditor.value, "text/xml");
-    // Здесь можно реализовать предварительный просмотр на основе xmlDoc
-    // Например, вы можете создать элементы и добавить их в preview.
-    preview.innerHTML = "Preview will be displayed here.";
+    // Очистите предварительный просмотр перед обновлением
+    preview.innerHTML = "";
+    // Создайте элементы предварительного просмотра на основе xmlDoc
+    createPreviewElements(preview, xmlDoc.documentElement);
+
+    // Обновите содержимое <textarea>
+    xmlEditor.value = new XMLSerializer().serializeToString(xmlDoc);
   } catch (error) {
+    console.log(error);
     preview.innerHTML = "Invalid XML. Please check your input.";
+  }
+}
+
+function createPreviewElements(container, element) {
+  const elementPreview = document.createElement("div");
+  elementPreview.className = "xml-element";
+  elementPreview.textContent = `<${element.nodeName}>`;
+
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add Child";
+  addButton.addEventListener("click", function () {
+    addXmlElement(element);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete Element";
+  deleteButton.addEventListener("click", function () {
+    deleteXmlElement(element);
+  });
+
+  elementPreview.appendChild(document.createElement("br"));
+
+  if (element.children.length == 0) {
+    const text4XMLelement = document.createElement("textarea");
+    text4XMLelement.className = "xml-input";
+    text4XMLelement.value = element.innerHTML;
+    elementPreview.appendChild(text4XMLelement);
+    elementPreview.appendChild(document.createElement("br"));
+  }
+
+  elementPreview.appendChild(addButton);
+  elementPreview.appendChild(deleteButton);
+  container.appendChild(elementPreview);
+
+  for (let i = 0; i < element.children.length; i++) {
+    createPreviewElements(container, element.children[i]);
+  }
+}
+
+function hideElement(element) {
+  for (let i = 0; i < element.children.length; i++) {
+    element.children[i].classList.add("hide");
   }
 }
 
@@ -52,3 +99,8 @@ function previewXml() {
 document.getElementById("xmlFileInput").addEventListener("change", loadXml);
 document.getElementById("xmlEditor").addEventListener("input", previewXml);
 document.getElementById("saveButton").addEventListener("click", saveXml);
+
+// Вызов функции предварительного просмотра при загрузке страницы
+window.onload = function () {
+  previewXml();
+};
